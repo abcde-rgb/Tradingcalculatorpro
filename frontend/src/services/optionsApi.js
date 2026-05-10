56e2f1c350a3843bdc528c29f3642424cc8cacd6
abcde-rgb <viti.fisas@gmail.com>
2026-05-10 11:05:52 +0200

    Use normalized API URL in options service

--- frontend/src/services/optionsApi.js
+++ frontend/src/services/optionsApi.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
-
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+import { getApiBaseUrl } from '@/lib/api';
 
 const api = axios.create({
-  baseURL: API,
+  baseURL: getApiBaseUrl(),
   timeout: 10000,
 });
 
